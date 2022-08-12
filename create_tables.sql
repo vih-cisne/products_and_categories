8a793892fae742dea2c1f4348b089129ce087ac3
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-08-11 23:29:49 -0300

    feat: volumes

--- create_tables.sql
+++ create_tables.sql
@@ -1,9 +1,9 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS categories(
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(200) NOT NULL UNIQUE 
-)
+);
 
 CREATE TABLE IF NOT EXISTS products(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -11,4 +11,4 @@ CREATE TABLE IF NOT EXISTS products(
     price DECIMAL(8,2) NOT NULL,
     category_id INTEGER,
     FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
-)
+);
